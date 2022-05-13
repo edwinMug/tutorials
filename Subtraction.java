99eb0cadc388b851100aa732ed0c83f8d27a7944
Edwin Wanjau <82159066+edwinMug@users.noreply.github.com>
2022-05-13 13:12:39 +0300

    added subtraction

---
+++ Subtraction.java
@@ -0,0 +1,12 @@
+package tutorials;
+
+public class Subtraction {
+    int a =10;
+    int b=5;
+    int c =a-b;
+
+    public static void main(String[] args) {
+        Subtraction subtraction=new Subtraction();
+        System.out.println("subtraction = " + subtraction.c);
+    }
+}

0a87cb0b3fa4401d2fef71e052725a9bddf9bf7f
Edwin Wanjau <82159066+edwinMug@users.noreply.github.com>
2022-05-13 12:25:57 +0300

    first commit

---
+++ Addition.java
@@ -0,0 +1,16 @@
+package tutorials;
+
+public class Addition {
+
+
+    int a = 5;
+    int b = 10;
+    int c = a + b;
+
+    public static void main(String[] args) {
+
+        Addition addition = new Addition();
+
+        System.out.println("c = " + addition.c);
+    }
+}
